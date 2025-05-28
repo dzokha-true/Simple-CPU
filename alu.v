de708eecc8bb752301346a0bc7e35dbd634877ab
chena3537 <81980286+chena3537@users.noreply.github.com>
2025-05-28 16:34:52 +1000

    Update alu.v

--- alu.v
+++ alu.v
@@ -8,19 +8,19 @@ module alu (a, b, alu_op_select, result);
     parameter ALU_OP_PASS   = 3'b000;
     parameter ALU_OP_ADD    = 3'b010;
     parameter ALU_OP_XOR    = 3'b011;
-	 parameter ALU_OP_OR     = 3'b100;
-	 parameter ALU_OP_AND    = 3'b101;
+    parameter ALU_OP_OR     = 3'b100;
+    parameter ALU_OP_AND    = 3'b101;
 	 
 	 
     // parameter ALU_OP_UNUSED = 2'b11; // Future use
 
     always @(*) begin // Combinational
         case (alu_op_select)
-            ALU_OP_PASS_B: result = operand_b;
-            ALU_OP_ADD:    result = operand_a + operand_b;
-            ALU_OP_XOR:    result = operand_a ^ operand_b;
-				ALU_OP_OR:     result = operand_a | operand_b;
-				ALU_OP_AND:    result = operand_a & operand_b;
+            ALU_OP_PASS_B: result = b;
+            ALU_OP_ADD:    result = a + b;
+            ALU_OP_XOR:    result = a ^ b;
+	    ALU_OP_OR:     result = a | b;
+	    ALU_OP_AND:    result = a & b;
 				
             default:       result = 8'h00; // Default or error case
         endcase
