69b08ed08c975f9c889bc559a1d0c29957f5c372
chena3537 <81980286+chena3537@users.noreply.github.com>
2025-05-29 02:26:54 +1000

    Update controller.v

--- controller.v
+++ controller.v
@@ -1,18 +1,18 @@
-module controller(clk, rst, start, code, inst_reg, r_en_OH, tri_controller_OH, branch, inc_pc);
+module controller(clk, rst, start, code, inst_reg, r_en_OH, tri_controller_OH, inc_pc);
     input clk, rst, start;
     input [22:0] code, inst_reg;
     output [9:0] r_en_OH, tri_controller_OH;
-    output branch, inc_pc;
+    output inc_pc;
 
     wire [4:0] curr_state, next_state;
-    wire [3:0] r_en, tri_controller; 
+	 wire [3:0] r_en, tri_controller;
     wire end_instruction;
 
-    find_ns next(.state(curr_state), .code(code[22:20]), .next_state(next_state), .start(start));
+    find_ns next(.state(curr_state), .code(code[22:20]), .next_state(next_state), .start(start), .rst(rst));
 	 
-    state_reg state_register(.d(next_state), .clk(clk), .rst(rst), .q(curr_state));
+	 state_reg state_register(.d(next_state), .clk(clk), .rst(rst), .q(curr_state));
 	 
-    outputsig outsig(.state(curr_state), .instr(code), .r_en(r_en), .tribuf(tri_controller), .branch(branch));
+	 outputsig outsig(.state(curr_state), .instr(code), .r_en(r_en), .tribuf(tri_controller), .PC_step(inc_pc));
     
     //decoder
     binary_to_onehot r_en_decoder(.fourBit(r_en), .onehot(r_en_OH));
