403add3775687961383a320de7a36a1b0eedd88f
chena3537 <81980286+chena3537@users.noreply.github.com>
2025-05-28 22:15:41 +1000

    Update outputsig.v

--- outputsig.v
+++ outputsig.v
@@ -8,9 +8,9 @@ module outputsig(state, instr, branch, PC_step, tribuf, r_en);
 		case (state)
 			5'b00001: begin tribuf <= 4'd9;	r_en <= instr[19:16]; end //load
 			5'b00010: begin tribuf <= instr[15:12];	r_en <= instr[19:16]; end//mov
-			5'b00011: begin tribuf <= instr[19:16];	r_en <= 4'd10; end//arithmetic
-			5'b00100: begin tribuf <= instr[15:12];	r_en <= 4'd9; end
-			5'b00101: begin tribuf <= 4'd9;	        r_en <= instr[19:16]; end
+			5'b00011: begin tribuf <= instr[19:16];	r_en <= 4'd9; end//arithmetic
+			5'b00100: begin tribuf <= instr[15:12];	r_en <= 4'd8; end
+			5'b00101: begin tribuf <= 4'd8;	        r_en <= instr[19:16]; end
 			default:  begin tribuf <= 4'd0;	        r_en <= 4'd0; end
 		endcase
 	end
