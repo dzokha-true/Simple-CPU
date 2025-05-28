396365e8ab3b80a0bdbd314598fd204c9fe137a2
chena3537 <81980286+chena3537@users.noreply.github.com>
2025-05-28 22:56:48 +1000

    Update one_hot_decoder.v

--- one_hot_decoder.v
+++ one_hot_decoder.v
@@ -1,20 +1,20 @@
-module binary_to_onehot (fiveBit, onehot);
-    input [4:0] fiveBit; // 5-bit binary input
+module binary_to_onehot (fourBit, onehot);
+	input [3:0] fourBit; // 5-bit binary input
     output reg [19:0] onehot; // 32-bit one-hot output
 
     always @(fiveBit) begin
         // Set the corresponding bit in one-hot encoding
         case (fiveBit)
-            5'b00000: onehot <= 10'd1; // 11
-            5'b00001: onehot <= 10'd2; // 1
-            5'b00010: onehot <= 10'd4; // 2
-            5'b00011: onehot <= 10'd8; // 3
-            5'b00100: onehot <= 10'd16; // 4
-            5'b00101: onehot <= 10'd32; // 5
-            5'b00110: onehot <= 10'd64; // 6
-            5'b00111: onehot <= 10'd128; // 7
-            5'b01000: onehot <= 10'd256; // 8
-            5'b01001: onehot <= 10'd512; // 9
+            4'b0000: onehot <= 10'd1; // 11
+            4'b0001: onehot <= 10'd2; // 1
+            4'b0010: onehot <= 10'd4; // 2
+            4'b0011: onehot <= 10'd8; // 3
+            4'b0100: onehot <= 10'd16; // 4
+            4'b0101: onehot <= 10'd32; // 5
+            4'b0110: onehot <= 10'd64; // 6
+            4'b0111: onehot <= 10'd128; // 7
+            4'b1000: onehot <= 10'd256; // 8
+            4'b1001: onehot <= 10'd512; // 9
 			default: onehot <= 10'd0;
         endcase
     end
