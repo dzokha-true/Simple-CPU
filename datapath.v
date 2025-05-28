ce80da42c71cedb9ee288c92bd6cb42761dd0d2b
chena3537 <81980286+chena3537@users.noreply.github.com>
2025-05-29 02:27:27 +1000

    Update datapath.v

--- datapath.v
+++ datapath.v
@@ -1,15 +1,13 @@
-module datapath (clk, rst_n, r_en_OH, tri_controller_OH, code, address, bus);
+module datapath (clk, rst_n, r_en_OH, tri_controller_OH, code, bus);
     input clk;
     input rst_n;
     input [9:0] r_en_OH, tri_controller_OH;
     input [22:0] code;
-    input [5:0] address;
     output [15:0] bus;
 	 
 
     wire [15:0] reg_out[0:19]; // Output from registers
     wire [15:0] alu_out; // Output from ALU
-    wire [5:0] address_delayed;
 
     genvar i; 
     generate
@@ -33,7 +31,6 @@ module datapath (clk, rst_n, r_en_OH, tri_controller_OH, code, address, bus);
     endgenerate
 	 
 	register A_reg(.d(bus), .clk(clk), .rst(rst_n), .en(r_en_OH[9]), .q(reg_out[9]));
-	
     alu my_alu(
         .a(reg_out[9]),
         .b(bus),
