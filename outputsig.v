75e2e1adb8c2046455cec465600aece3467a6322
chena3537 <81980286+chena3537@users.noreply.github.com>
2025-05-28 16:09:41 +1000

    Update outputsig.v

--- outputsig.v
+++ outputsig.v
@@ -6,28 +6,18 @@ module outputsig(state, opcode, inst_reg, branch, PC_step, tribuf, r_en);
 	
 	always @(opcode, state) begin
 		case (state)
-			5'b00001:
-				begin tribuf <= 4'd10;	   r_en <= opcode[19:16]; //load 
-			end
-			5'b00010:
-				begin tribuf <= opcode[15:12];	r_en <= opcode[19:16]; //mov
-			end
-			5'b00011:
-				begin tribuf <= opcode[19:16];	r_en <= 4'd10; //arithmetic
-			end
-			5'b00100:
-				begin tribuf <= opcode[15:12];	r_en <= 4'd9;
-			end
-			5'b00101:
-				begin tribuf <= 4'd9;	r_en <= opcode[19:16];
-			end
-			default:  begin tribuf <= 4'd0;	r_en <= 4'd0;
-			end
+			5'b00001: begin tribuf <= 4'd10;	r_en <= instr[19:16]; //load
+			5'b00010: begin tribuf <= instr[15:12];	r_en <= instr[19:16]; //mov
+			5'b00011: begin tribuf <= instr[19:16];	r_en <= 4'd10; //arithmetic
+			5'b00100: begin tribuf <= instr[15:12];	r_en <= 4'd9;
+			5'b00101: begin tribuf <= 4'd9;	        r_en <= instr[19:16];
+			default:  begin tribuf <= 4'd0;	        r_en <= 4'd0;
 		endcase
 	end
 	always @(state) begin
 		case (state)
 			5'b00001, 5'b00001, 5'b00001: PC_step <= 1'b1;
+			default: PC_step <= 1'b0;
 		endcase
 	end
 endmodule
