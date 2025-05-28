d0c00f647cb316d907573e655b420ae7644fbb60
chena3537 <81980286+chena3537@users.noreply.github.com>
2025-05-29 02:28:28 +1000

    Update find_ns.v

--- find_ns.v
+++ find_ns.v
@@ -5,7 +5,7 @@ module find_ns(state, code, rst, start, next_state);
 	input rst, start;
 	output reg[4:0] next_state;
 	
-always @(rst, state, start, code) begin
+	always @(rst, state, start, code) begin
 		if (rst == 1'b1)
 			next_state <= 5'b11111;
 		else begin
@@ -19,18 +19,17 @@ always @(rst, state, start, code) begin
 							3'b011:  next_state <= 5'b00011; //xor
 							3'b100:  next_state <= 5'b00011; //or
 							3'b101:  next_state <= 5'b00011; //and
-							3'b110:  next_state <= 5'b00110; //branch
 							default: next_state <= 5'b10000;
 						endcase
 					end
-				5'b00001, 5'b00010, 5'b00101, 5'b00110: next_state <= 5'b10000;
+				5'b00001, 5'b00010: next_state <= 5'b10000;
 				5'b00011: next_state <= 5'b00100;
 				5'b00100: next_state <= 5'b00101;
 		
 				
 				
 				
-				5'b10000: next_state <= 5'b00000;
+				5'b00101, 5'b10000: next_state <= 5'b00000;
 				5'b11111: if (start == 1'b1)
 								next_state <= 5'b10000;
 							 else
