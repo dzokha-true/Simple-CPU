451cab1fac9fd6a4f34c46e5c649a65068aa9398
chena3537 <81980286+chena3537@users.noreply.github.com>
2025-05-28 16:07:45 +1000

    Update datapath.v

--- datapath.v
+++ datapath.v
@@ -4,7 +4,8 @@ module datapath (clk, rst_n, r_en_OH, tri_controller_OH, code, address, bus);
     input [19:0] r_en_OH, tri_controller_OH;
     input [22:0] code;
     input [5:0] address;
-    output [15:0] bus;
+    ouput [15:0] bus;
+	 
 
     wire [15:0] reg_out[0:19]; // Output from registers
     wire [15:0] alu_out; // Output from ALU
@@ -25,16 +26,16 @@ module datapath (clk, rst_n, r_en_OH, tri_controller_OH, code, address, bus);
         for (i = 0; i<8; i=i+1) begin : tri_gen
             tri_buf my_tris(
                 .enable(tri_controller_OH[i]),
-                .a(bus),
-                .b(reg_out[i])
+                .a(reg_out[i]),
+                .b(bus)
             );
         end
     endgenerate
 	 
 	register A_reg(.d(bus), .clk(clk), .rst(rst_n), .en(r_en_OH[10]), .q(reg_out[10]));
     alu my_alu(
-        .operand_a(reg_out[1]),
-        .operand_b(reg_out[2]),
+        .operand_a(reg_out[10]),
+        .operand_b(bus),
         .alu_op_select(code[22:20]),
         .result(alu_out)
     );
@@ -48,7 +49,7 @@ module datapath (clk, rst_n, r_en_OH, tri_controller_OH, code, address, bus);
     
     // tri_buf for bus
     tri_buf bus_tri(
-        .enable(tri_controller_OH[11]),
+        .enable(tri_controller_OH[10]),
         .a(code[15:0]),
         .b(bus)
     );
