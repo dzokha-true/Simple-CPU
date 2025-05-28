22756af5357071b34f3487662cf90b0532612c08
chena3537 <81980286+chena3537@users.noreply.github.com>
2025-05-29 02:29:28 +1000

    Update simple_processor.v

--- simple_processor.v
+++ simple_processor.v
@@ -1,46 +1,38 @@
 module simple_processor(reset, clk, start, write, program_in);
     input reset, clk, start, write;
     input[22:0] program_in;
-
+	 
+	 wire [5:0] address;
     wire [9:0] r_en_OH, tri_controller_OH;
     wire [15:0] bus;
-    wire inc_PC, branch;
-    wire [5:0] address;
-    wire [22:0] code, inst_reg;
+    wire inc_pc;
+    wire [22:0] code,inst_reg;
+	 
+	 
 
 controller my_controller(
     .clk(clk), 
     .rst(reset), 
     .start(start), 
-    .code(program_in), 
+    .code(code), 
     .inst_reg(inst_reg), 
     .r_en_OH(r_en_OH), 
     .tri_controller_OH(tri_controller_OH), 
-    .branch(branch), 
-    .inc_pc(inc_PC)
+    .inc_pc(inc_pc)
 );
+
 
 datapath my_datapath(
     .clk(clk), 
     .rst_n(reset), 
     .r_en_OH(r_en_OH), 
-    .tri_controller_OH(tri_controller_OH), 
-    .bus(bus), 
-    .address(address), 
-    .code(program_in)
+    .tri_controller_OH(tri_controller_OH),
+    .bus(bus),
+    .code(code)
 );
-/*
-pc my_pc(
-    .bus(bus), 
-    .clk(clk), 
-    .rst(reset), 
-    .start(start), 
-    .inc_pc(inc_PC), 
-    .write(write), 
-    .address(address)
-);
-*/
 
-//decoder
+pc program_counter(.clk(clk), .rst(reset), .inc_pc(inc_pc), .start(start), .address(address));
+
+fake_IM im(.address(address),.code(code));
 
 endmodule
