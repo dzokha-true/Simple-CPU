17d29a8bf64fad798183eb1bfd83165da42af826
chena3537 <81980286+chena3537@users.noreply.github.com>
2025-05-28 16:07:15 +1000

    Update controller.v

--- controller.v
+++ controller.v
@@ -1,14 +1,17 @@
 module controller(clk, rst, start, code, inst_reg, r_en_OH, tri_controller_OH, branch, inc_pc);
     input clk, rst, start;
     input [22:0] code, inst_reg;
-    output [19:0] r_en_OH, tri_controller_OH;
+    output [9:0] r_en_OH, tri_controller_OH;
     output branch, inc_pc;
 
     wire [4:0] curr_state, next_state, r_en, tri_controller;
     wire end_instruction;
 
     find_ns next(.state(curr_state), .opcode(code[22:20]), .next_state(next_state), .start(start));
-	outputsig outsig(.state(curr_state), .opcode(code[22:20]), .r_en(r_en), .tribuf(tri_controller), .branch(branch));
+	 
+	 
+	 
+	 outputsig outsig(.state(curr_state), .instr(code), .r_en(r_en), .tribuf(tri_controller), .branch(branch));
     
     //decoder
     binary_to_onehot r_en_decoder(.fiveBit(r_en), .onehot(r_en_OH));
